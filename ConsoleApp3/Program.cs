c6831fde227bb7c2c93bd87ba29469a803aeed5f
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-01-31 14:38:15 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Books Test = new Books("Белые ночи", "Достоевский", 1848, 62);
+            Test.Info();
+            Console.ReadLine();
+
+        }
+        class Books
+        {
+            public string name;
+            public string author;
+            public int publication;
+            public int volume;
+
+            public Books(string name, string author, int publication, int volume)
+            {
+                this.name = name;
+                this.author = author;
+                this.publication = publication;
+                this.volume = volume;
+            }
+            public void Info()
+            {
+                Console.WriteLine("Информация о книге:");
+                Console.WriteLine($"Название: {name}");
+                Console.WriteLine($"Автор: {author}");
+                Console.WriteLine($"Год выпуска: {publication}");
+                Console.WriteLine($"Объем: {volume} страниц");
+            }
+        }
+    }
+}
